d2e882b9d49c41c95060b7380941d1e97f9d0328
orasaplarr <orasaplar@gmail.com>
2023-10-06 19:53:01 +0700

    เซฟการจองได้ แต่ยังเซฟ usernameคนจองไม่ได้

--- controllers/booking-userController.js
+++ controllers/booking-userController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
 const Service = require('../models/Service');
 const Home = require('../models/Home-Admin');
+const QueueBookingUser = require('../models/Queuebooking-User');
 
 exports.showBookingUser = async(req, res) => {
     const HomeData = await Home.find();
@@ -15,6 +16,28 @@ exports.showBookingUser = async(req, res) => {
         });
        
     });
-    
+};
 
-}
+exports.addBookingUser = async (req,res) =>{ 
+    try{
+        const user = await User.findById(req.session.userId);
+        if(!user){
+            return res.status(404).json({error:'User not found'});
+        }
+        
+        
+        const bookingCustomer =  new QueueBookingUser({
+        
+        booking_date_user : req.body.booking_date_user,
+        booking_service_user :  req.body.booking_service_user,
+        booking_time_user :  req.body.booking_time_user,
+        }); 
+        
+       
+       bookingCustomer.save();
+       console.log("Create service successfully!")
+       res.redirect('/booking-user');
+    } catch(error){
+        console.log("Error")
+    }
+};
---
+++ public/js/dateBooking.js
@@ -0,0 +1,125 @@
+var date1 = new Date();
+var dayNow =  date1.getDay();
+var dateNow =  date1.getDate();
+var monthNow =  date1.getMonth()+1; 
+var yearNow =  date1.getFullYear(); 
+
+function min(){
+if(monthNow <= 9 && dateNow <= 9){
+   var t1 = yearNow + '-0' + monthNow +'-0'+ dateNow ;
+   document.getElementById("dateBookingUser").setAttribute("min",t1);
+}else if(monthNow <= 9 && dateNow > 9){
+   var t2 = yearNow + '-0' + monthNow +'-'+ dateNow ;
+   document.getElementById("dateBookingUser").setAttribute("min",t2);
+}else if(dateNow <= 9 && monthNow > 9 ){
+   var t3 = yearNow + '-' + monthNow +'-0'+ dateNow ;
+   document.getElementById("dateBookingUser").setAttribute("min",t3);
+}else if(monthNow > 9 && dateNow > 9){
+   var t4 = yearNow + '-' + monthNow +'-'+ dateNow ;
+   document.getElementById("dateBookingUser").setAttribute("min",t4);
+}
+};
+
+
+var t6,t7,t8,t9,t10,t11,t12;
+
+function max(){
+  if(dayNow === 1){
+     dateNow = dateNow+6
+     if(monthNow <= 9 && dateNow <= 9){
+        t6 = yearNow + '-0' + monthNow +'-0'+ dateNow ;
+     }else if (monthNow <= 9 && dateNow > 9){
+        t6 = yearNow + '-0' + monthNow +'-'+ dateNow ;
+     }else if(dateNow <= 9 && monthNow > 9 ){
+        t6 = yearNow + '-' + monthNow +'-0'+ dateNow ;
+     }else if(monthNow > 9 && dateNow > 9){
+        t6 = yearNow + '-' + monthNow +'-'+ dateNow ;
+     }
+     document.getElementById("dateBookingUser").setAttribute("max",t6);
+
+
+  }else if(dayNow === 2){
+     dateNow = dateNow + 5
+     if(monthNow <= 9 && dateNow <= 9){
+        t7 = yearNow + '-0' + monthNow +'-0'+ dateNow ;
+     }else if (monthNow <= 9 && dateNow > 9){
+        t7 = yearNow + '-0' + monthNow +'-'+ dateNow ;
+     }else if(dateNow <= 9 && monthNow > 9 ){
+        t7 = yearNow + '-' + monthNow +'-0'+ dateNow ;
+     }else if(monthNow > 9 && dateNow > 9){
+        t7 = yearNow + '-' + monthNow +'-'+ dateNow ;
+     }  
+     document.getElementById("dateBookingUser").setAttribute("max",t7);
+
+  }else if(dayNow === 3){
+   dateNow = dateNow + 4 //วันอาทิตย์
+     if(monthNow <= 9 && dateNow <= 9   ){
+        t8 = yearNow + '-0' + monthNow +'-0'+ dateNow ;
+     }else if (monthNow <= 9 && dateNow > 9 ){
+        t8 = yearNow + '-0' + monthNow +'-'+ dateNow ;
+     }else if(dateNow <= 9 && monthNow > 9 ){
+        t8 = yearNow + '-' + monthNow +'-0'+ dateNow ;
+     }else if(monthNow > 9 && dateNow > 9 ){
+        t8 = yearNow + '-' + monthNow +'-'+ dateNow ;
+     }  
+     document.getElementById("dateBookingUser").setAttribute("max",t8);
+
+  }else if(dayNow === 4){
+     dateNow = dateNow + 3
+     if(monthNow <= 9 && dateNow <= 9){
+        t9 = yearNow + '-0' + monthNow +'-0'+ dateNow ;
+     }else if (monthNow <= 9 && dateNow > 9){
+        t9 = yearNow + '-0' + monthNow +'-'+ dateNow ;
+     }else if(dateNow <= 9 && monthNow > 9 ){
+        t9 = yearNow + '-' + monthNow +'-0'+ dateNow ;
+     }else if(monthNow > 9 && dateNow > 9){
+        t9 = yearNow + '-' + monthNow +'-'+ dateNow ;
+     }  
+     document.getElementById("dateBookingUser").setAttribute("max",t9);
+
+  }else if(dayNow === 5){
+     dateNow = dateNow + 2
+     if(monthNow <= 9 && dateNow <= 9){
+        t10 = yearNow + '-0' + monthNow +'-0'+ dateNow ;
+     }else if (monthNow <= 9 && dateNow > 9){
+        t10 = yearNow + '-0' + monthNow +'-'+ dateNow ;
+     }else if(dateNow <= 9 && monthNow > 9 ){
+        t10 = yearNow + '-' + monthNow +'-0'+ dateNow ;
+     }else if(monthNow > 9 && dateNow > 9){
+        t10 = yearNow + '-' + monthNow +'-'+ dateNow ;
+     }  
+     document.getElementById("dateBookingUser").setAttribute("max",t10);
+
+  }else if(dayNow === 6){
+     dateNow = dateNow + 1
+     if(monthNow <= 9 && dateNow <= 9){
+        t11 = yearNow + '-0' + monthNow +'-0'+ dateNow ;
+     }else if (monthNow <= 9 && dateNow > 9){
+        t11 = yearNow + '-0' + monthNow +'-'+ dateNow ;
+     }else if(dateNow <= 9 && monthNow > 9 ){
+        t11 = yearNow + '-' + monthNow +'-0'+ dateNow ;
+     }else if(monthNow > 9 && dateNow > 9){
+        t11 = yearNow + '-' + monthNow +'-'+ dateNow ;
+     }  
+     document.getElementById("dateBookingUser").setAttribute("max",t11);
+
+  }else if(dayNow === 7){
+     
+     if(monthNow <= 9 && dateNow <= 9){
+        t12 = yearNow + '-0' + monthNow +'-0'+ dateNow ;
+     }else if (monthNow <= 9 && dateNow > 9){
+        t12 = yearNow + '-0' + monthNow +'-'+ dateNow ;
+     }else if(dateNow <= 9 && monthNow > 9 ){
+        t12 = yearNow + '-' + monthNow +'-0'+ dateNow ;
+     }else if(monthNow > 9 && dateNow > 9){
+        t12 = yearNow + '-' + monthNow +'-'+ dateNow ;
+     }  
+     document.getElementById("dateBookingUser").setAttribute("max",t12);
+  }
+
+
+};
+
+min();
+max();
+
