345e5329011d7bfd59829723d86815126aa518a4
Pitchaporn Chottitammasuk <139955220+Pployy09@users.noreply.github.com>
2023-10-17 22:23:23 +0700

    แก้noacc

--- Middleware/adminMiddleware.js
+++ Middleware/adminMiddleware.js
@@ -2,12 +2,26 @@ const User = require('../models/User')
 
 module.exports = (req, res, next) => {
     User.findById(req.session.userId).then((user) => {
+        if(user.role == 'admin'){
+            next();
+        }else {
+          res.redirect('/no-access'); // ถ้าไม่ใช่บทบาท "admin" เรียกหน้าที่ไม่ได้รับอนุญาต
+        }
+        console.log('successfully')
+        
+    })
+}
+
+/*
+module.exports = (req, res, next) => {
+    User.findById(req.session.userId).then((user) => {
         if (user.role == 'admin') {
             next();
         } else {
             // ใช้ JavaScript เพื่อแสดงแจ้งเตือนและกลับไปหน้าก่อนหน้า
-            res.send('<script>alert("คุณไม่ได้รับอนุญาตในส่วนนี้"); window.history.go(-1);</script>');
+            res.send('<script>alert("คุณไม่ได้รับอนุญาตให้เข้าถึงหน้านี้"); window.history.go(-1);</script>');
         }
         console.log('successfully');
     });
 }
+*/
