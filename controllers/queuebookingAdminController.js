6e16f11e2d98a85d8a14d1801bcd24051a057996
Pitchaporn Chottitammasuk <139955220+Pployy09@users.noreply.github.com>
2023-11-05 20:44:20 +0700

    แก้จองคิวเลือกพนักงาน

--- controllers/queuebookingAdminController.js
+++ controllers/queuebookingAdminController.js
@@ -10,18 +10,17 @@ exports.showQueuebooking = async(req, res) => {
     const ServiceData = await Service.find();
     const HomeData = await Home.find();
     const QueueBookingCustomerData = await QueueBookingCustomer.find();
-    await  User.find({ role : "emp"}).then(function(emps){
+    await User.find({ role : "emp"}).then(function(emps){
         res.render("queuebooking-admin",{
-            empList : emps,
-            UserData:UserData,
+            empList         : emps,
+            UserData        : UserData,
             ServiceDataList : ServiceData,
-            HomeData : HomeData,
-            QueueBookingCustomerData:QueueBookingCustomerData,
+            HomeData        : HomeData,
+            QueueBookingCustomerData : QueueBookingCustomerData,
         });
     });
     
 }
-
 
 // add booking
 exports.addQueuebooking = async (req, res) => {
@@ -38,8 +37,8 @@ exports.addQueuebooking = async (req, res) => {
         // ตรวจสอบว่ามีการจองเวลาและพนักงานเหมือนกันหรือไม่
         const existingBookings = await QueueBookingCustomer.find({
             booking_date_user,
-            booking_time_user: { $in: booking_time_user },
-            employeeName: { $in: employeeName }
+            booking_time_user   : { $in: booking_time_user },
+            employeeName        : { $in: employeeName }
         });
 
         if (existingBookings.length > 0) {
@@ -55,7 +54,7 @@ exports.addQueuebooking = async (req, res) => {
             booking_service_user,
             booking_time_user,
             employeeName,
-            emp_selection_status: "เลือกพนักงานแล้ว",
+            emp_selection_status : "เลือกพนักงานแล้ว",
         });
 
         await bookingCustomer.save();
@@ -69,46 +68,65 @@ exports.addQueuebooking = async (req, res) => {
 
 
 //edit booking
-exports.editQueuebooking = async (req, res) =>{
-    try{
+exports.editQueuebooking = async (req, res) => {
+    try {
         const UserData = await User.findById(req.session.userId);
         const HomeData = await Home.find();
         const ServiceData = await Service.find();
-        const QueueBookingCustomerData = await QueueBookingCustomer.findOne({ _id : req.params.id});
-        await  User.find({ role : "emp"}).then(function(emps){
-            res.render('edit-queuebooking',{
-                UserData : UserData,
-                QueueBookingCustomerData : QueueBookingCustomerData,
-                HomeData : HomeData,
-                ServiceDataList : ServiceData,
-                empList : emps,
-                
+        const QueueBookingCustomerData = await QueueBookingCustomer.findOne({ _id: req.params.id });
+        await User.find({ role: "emp" }).then(function (emps) {
+            res.render('edit-queuebooking', {
+                UserData: UserData,
+                QueueBookingCustomerData: QueueBookingCustomerData,
+                HomeData: HomeData,
+                ServiceDataList: ServiceData,
+                empList: emps,
             })
         });
-       
-    }catch(error){
+
+    } catch (error) {
         console.log("Error")
     }
 }
 
 //update booking
-exports.editPutQueuebooking = async (req,res) =>{
-    try{
-        await QueueBookingCustomer.findByIdAndUpdate(req.params.id,{
-            customerUsername:req.body.customerUsername,
-            customerFirstname : req.body.firstname,
-            customerLastname:req.body.lastname,
-            phonenum : req.body.phonenum,
-            booking_date_user : req.body.booking_date_user,
-            booking_service_user :  req.body.booking_service_user,
-            booking_time_user :  req.body.booking_time_user,
-            employeeName : req.body.employeeName,
-            emp_selection_status : "เลือกพนักงานแล้ว" ,
-            price : req.body.price,
-        })
-        res.redirect('/queuebooking-admin');
+exports.editPutQueuebooking = async (req, res) => {
+    try {
+        const {
+            booking_date_user,
+            booking_time_user,
+            employeeName
+        } = req.body;
 
-    }catch(error){
+        // ตรวจสอบว่ามีการจองเวลาและพนักงานเหมือนกันหรือไม่
+        const existingBookings = await QueueBookingCustomer.find({
+            booking_date_user,
+            booking_time_user,
+            employeeName,
+            _id: { $ne: req.params.id } 
+        });
+
+        if (existingBookings.length > 0) {
+            res.send("<script>alert('พนักงานคนนี้มีคิวเต็มแล้ว! กรุณาเลือกพนักงานคนอื่นหรือเปลี่ยนเวลาในการจองคิว'); window.location = '/queuebooking-admin';</script>");
+            return;
+        }
+
+        // อัปเดตการจองลูกค้า
+        await QueueBookingCustomer.findByIdAndUpdate(req.params.id, {
+            customerUsername: req.body.customerUsername,
+            customerFirstname: req.body.firstname,
+            customerLastname: req.body.lastname,
+            phonenum: req.body.phonenum,
+            booking_date_user: req.body.booking_date_user,
+            booking_service_user: req.body.booking_service_user,
+            booking_time_user: req.body.booking_time_user,
+            employeeName: req.body.employeeName,
+            emp_selection_status: "เลือกพนักงานแล้ว",
+            price: req.body.price,
+        });
+
+        res.redirect('/queuebooking-admin');
+    } catch (error) {
         console.log(error);
     }
 };
@@ -120,8 +138,7 @@ exports.deleteQueuebooking = async (req,res) =>{
        
         res.redirect('/queuebooking-admin');
         
-
-    }catch(error){
+    } catch(error){
         console.log(error);
     }
 };
